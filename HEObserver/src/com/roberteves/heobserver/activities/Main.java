99210ff4899957f5da319a3b83409231da18259e
Robert Eves <roberteves92@gmail.com>
2015-01-16 10:54:58 +0000

    added listview declaration

--- HEObserver/src/com/roberteves/heobserver/activities/Main.java
+++ HEObserver/src/com/roberteves/heobserver/activities/Main.java
@@ -6,15 +6,17 @@ import com.roberteves.heobserver.R;
 import android.app.Activity;
 import android.os.Bundle;
 import android.os.StrictMode;
-import android.widget.TextView;
+import android.widget.ListView;
 
 public class Main extends Activity {
+private static ListView lv;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		Global.APP_CONTEXT = getApplicationContext();
+		lv = (ListView) findViewById(R.id.listView);
 
 		// TODO Removed and setup async feed methods
 		StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
