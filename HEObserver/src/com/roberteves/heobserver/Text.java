fda2779848b679d507c634c51c609e303bd10912
Robert Eves <roberteves92@gmail.com>
2015-01-20 19:09:43 +0000

    added method to select text from regex and added some processing for article text

--- HEObserver/src/com/roberteves/heobserver/Text.java
+++ HEObserver/src/com/roberteves/heobserver/Text.java
@@ -1,34 +1,38 @@
 package com.roberteves.heobserver;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.unbescape.html.HtmlEscape;
 
 public class Text {
 	private static String regexArticle = "(<!-- Article Start -->)([\\w\\d\\s\\W\\D\\S]+)(<!-- Article End -->)";
 	private static String regexHtml = "</?\\w+((\\s+\\w+(\\s*=\\s*(?:\".*?\"|'.*?'|[^'\">\\s]+))?)+\\s*|\\s*)/?>";
-	
+	private static String regexParagraph = "(</p>)";
+
 	public static String unescapeHtml(String title) {
 		return HtmlEscape.unescapeHtml(title);
 	}
-	
-	public static String processArticlePreview(String text)
-	{
+
+	public static String processArticlePreview(String text) {
 		// TODO 1.0 Un-capitalise first description word
-		
+
 		String t = text;
 		t = unescapeHtml(t);
-		t=t.replaceAll(regexHtml, ""); //remove any remaining html tags
+		t = t.replaceAll(regexHtml, ""); // remove any remaining html tags
 		return t;
 	}
-	
-	public static String processArticle(String text)
-	{
-		String t = text;
-		//TODO 1.0 Insert processing here
+
+	public static String processArticle(String text) {
+		String t = selectStringFromRegex(text, regexArticle);
+		t = t.replaceAll(regexParagraph, "\r\n");// add new lines
+		t = t.replaceAll(regexHtml, "");// remove any remaining html tags
 		return t;
 	}
 
@@ -37,5 +41,19 @@ public class Text {
 		Calendar calendar = new GregorianCalendar();
 		calendar.setTime(pubDate);
 		return sdf.format(calendar.getTime());
+	}
+	
+	private static String selectStringFromRegex(String text, String regex) {
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(text);
+		List<String> listMatches = new ArrayList<String>();
+		while (matcher.find()) {
+			listMatches.add(matcher.group());
+		}
+		String t = "";
+		for (String s : listMatches) {
+			t = t + s;
+		}
+		return t;
 	}
 }
