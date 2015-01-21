13acfeb04add322941a537957cd07edd37318eed
Robert Eves <roberteves92@gmail.com>
2015-01-21 13:47:01 +0000

    added blank activity

---
+++ HEObserver/src/com/roberteves/heobserver/activities/Article.java
@@ -0,0 +1,17 @@
+package com.roberteves.heobserver.activities;
+
+import com.roberteves.heobserver.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Article extends Activity {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_article);
+		
+	}
+
+}
