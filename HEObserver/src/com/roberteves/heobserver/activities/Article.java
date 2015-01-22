41f8462e9ebe38e4bd89cb1ec36d57eff02598c3
Robert Eves <roberteves92@gmail.com>
2015-01-22 11:16:13 +0000

    added text views

--- HEObserver/src/com/roberteves/heobserver/activities/Article.java
+++ HEObserver/src/com/roberteves/heobserver/activities/Article.java
@@ -4,14 +4,18 @@ import com.roberteves.heobserver.R;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Article extends Activity {
+	TextView txtTitle, txtBody;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
-	    super.onCreate(savedInstanceState);
+		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_article);
-		
+
+		txtTitle = (TextView) findViewById(R.id.txtTitle);
+		txtBody = (TextView) findViewById(R.id.txtBody);
 	}
 
 }
