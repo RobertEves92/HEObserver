f4b9e5c438b8e2a5ca229c6d4a27d43a10527272
Robert Eves <roberteves92@gmail.com>
2015-01-30 17:03:40 +0000

    if not match, open in webview

--- HEObserver/src/com/roberteves/heobserver/activities/WebActivity.java
+++ HEObserver/src/com/roberteves/heobserver/activities/WebActivity.java
@@ -1,10 +1,17 @@
 package com.roberteves.heobserver.activities;
 
+import com.roberteves.heobserver.R;
+
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.webkit.WebView;
 
+@SuppressLint("SetJavaScriptEnabled")
 public class WebActivity extends Activity {
+	private static WebView webView;
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -14,11 +21,15 @@ public class WebActivity extends Activity {
 				.getDataString()
 				.matches(
 						"http:\\/\\/((www.)?)hertsandessexobserver.co.uk\\/.*story.html")) {
-			// is article - open
+			// is article - open in article activity
 			// TODO Open in app
 		} else {
-			// is not article - close app
-			//TODO Open in browser
+			// is not article - open in web view
+			setContentView(R.layout.activity_web);
+
+			webView = (WebView) findViewById(R.id.webView);
+			webView.getSettings().setJavaScriptEnabled(true);
+			webView.loadUrl(intent.getDataString());
 		}
 	}
 }
