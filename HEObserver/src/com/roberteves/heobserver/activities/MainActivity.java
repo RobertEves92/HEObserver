5afc00bc054f159d58bb553a532002b02b9f6ae5
Robert Eves <roberteves92@gmail.com>
2015-01-30 15:27:53 +0000

    added internet connection check and manifest permission, refactored strings

--- HEObserver/src/com/roberteves/heobserver/activities/MainActivity.java
+++ HEObserver/src/com/roberteves/heobserver/activities/MainActivity.java
@@ -25,7 +25,10 @@ import com.roberteves.heobserver.core.Article;
 import com.roberteves.heobserver.core.Lists;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.view.Menu;
@@ -51,84 +54,94 @@ public class MainActivity extends Activity {
 	}
 
 	private void updateList() {
-		StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
-				.permitAll().build();
-		StrictMode.setThreadPolicy(policy);
+		if (isOnline()) {
+			StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
+					.permitAll().build();
+			StrictMode.setThreadPolicy(policy);
 
-		// Stores all Rss Items from news feed
-		try {
-			Lists.RssItems = getFeeds();
-			Lists.storyList = new ArrayList<Map<String, String>>();
+			// Stores all Rss Items from news feed
+			try {
+				Lists.RssItems = getFeeds();
+				Lists.storyList = new ArrayList<Map<String, String>>();
 
-			// Add all story items to hashmap array
-			for (RssItem item : Lists.RssItems) {
-				// If the article is a picture slideshow, dont add it to the
-				// list
-				if (!item.getTitle().toUpperCase().contains("PICTURES:")) {
-					Lists.storyList.add(createStory("story",
-							HtmlEscape.unescapeHtml(item.getTitle())));
-				}
-			}
-
-			SimpleAdapter simpleAdpt = new SimpleAdapter(this, Lists.storyList,
-					android.R.layout.simple_list_item_1,
-					new String[] { "story" }, new int[] { android.R.id.text1 });
-
-			lv.setAdapter(simpleAdpt);
-
-			lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-
-				@Override
-				public void onItemClick(AdapterView<?> parent, View view,
-						int position, long id) {
-
-					Article article;
-					try {
-						article = new Article(Lists.RssItems.get(position)
-								.getLink(), Lists.RssItems.get(position)
-								.getDescription(), Lists.RssItems.get(position)
-								.getPubDate());
-
-						Intent i = new Intent(MainActivity.this,
-								ArticleActivity.class);
-						
-						i.putExtra("article", article);
-						startActivity(i);
-					} catch (IOException e) {
-						Crashlytics.logException(e); // Send caught exception to
-														// crashlytics
-						Toast.makeText(getApplicationContext(),
-								"Error retrieving article from source",
-								Toast.LENGTH_SHORT).show();
+				// Add all story items to hashmap array
+				for (RssItem item : Lists.RssItems) {
+					// If the article is a picture slideshow, dont add it to the
+					// list
+					if (!item.getTitle().toUpperCase().contains("PICTURES:")) {
+						Lists.storyList.add(createStory("story",
+								HtmlEscape.unescapeHtml(item.getTitle())));
 					}
 				}
-			});
 
-			// lv.setOnItemLongClickListener(new
-			// AdapterView.OnItemLongClickListener() {
-			//
-			// @Override
-			// public boolean onItemLongClick(AdapterView<?> parent, View view,
-			// int position, long id) {
-			// Global.APP_CONTEXT = getApplicationContext();
-			//
-			// Dialogs.DisplayInfoAlert(
-			// "Article Summary",
-			// Text.processArticlePreview(Lists.RssItems.get(position)
-			// .getDescription())
-			// + "\r\n"
-			// + String.format(
-			// getString(R.string.published),
-			// Text.processPubDate(Lists.RssItems.get(
-			// position).getPubDate())),
-			// MainActivity.this);
-			// return true;
-			// }
-			// });
-		} catch (Exception e) {
-			Crashlytics.logException(e); // Send caught exception to crashlytics
-			Toast.makeText(getApplicationContext(),
-					"Error updating article list", Toast.LENGTH_SHORT).show();
+				SimpleAdapter simpleAdpt = new SimpleAdapter(this,
+						Lists.storyList, android.R.layout.simple_list_item_1,
+						new String[] { "story" },
+						new int[] { android.R.id.text1 });
+
+				lv.setAdapter(simpleAdpt);
+
+				lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+					@Override
+					public void onItemClick(AdapterView<?> parent, View view,
+							int position, long id) {
+
+						Article article;
+						try {
+							article = new Article(Lists.RssItems.get(position)
+									.getLink(), Lists.RssItems.get(position)
+									.getDescription(), Lists.RssItems.get(
+									position).getPubDate());
+
+							Intent i = new Intent(MainActivity.this,
+									ArticleActivity.class);
+
+							i.putExtra("article", article);
+							startActivity(i);
+						} catch (IOException e) {
+							Crashlytics.logException(e); // Send caught
+															// exception to
+															// crashlytics
+							Toast.makeText(getApplicationContext(),
+									R.string.error_retreive_article_source,
+									Toast.LENGTH_SHORT).show();
+						}
+					}
+				});
+
+				// lv.setOnItemLongClickListener(new
+				// AdapterView.OnItemLongClickListener() {
+				//
+				// @Override
+				// public boolean onItemLongClick(AdapterView<?> parent, View
+				// view,
+				// int position, long id) {
+				// Global.APP_CONTEXT = getApplicationContext();
+				//
+				// Dialogs.DisplayInfoAlert(
+				// "Article Summary",
+				// Text.processArticlePreview(Lists.RssItems.get(position)
+				// .getDescription())
+				// + "\r\n"
+				// + String.format(
+				// getString(R.string.published),
+				// Text.processPubDate(Lists.RssItems.get(
+				// position).getPubDate())),
+				// MainActivity.this);
+				// return true;
+				// }
+				// });
+			} catch (Exception e) {
+				Crashlytics.logException(e); // Send caught exception to
+												// crashlytics
+				Toast.makeText(getApplicationContext(),
+						R.string.error_update_article_list, Toast.LENGTH_SHORT)
+						.show();
+			}
+		} else {
+			Toast.makeText(getApplicationContext(), R.string.error_no_internet,
+					Toast.LENGTH_SHORT).show();
 		}
 	}
 
@@ -195,5 +208,11 @@ public class MainActivity extends Activity {
 		story.put(key, title);
 
 		return story;
+	}
+
+	private boolean isOnline() {
+		ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+		NetworkInfo netInfo = cm.getActiveNetworkInfo();
+		return netInfo != null && netInfo.isConnected();
 	}
 }
