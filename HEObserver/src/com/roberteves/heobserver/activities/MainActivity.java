0a635c045233558672061a88d34acfb629d37268
Robert Eves <roberteves92@gmail.com>
2015-02-03 17:37:50 +0000

    re-ordered Activity class (methods moved to Async task and placed in chronological order)

--- HEObserver/src/com/roberteves/heobserver/activities/MainActivity.java
+++ HEObserver/src/com/roberteves/heobserver/activities/MainActivity.java
@@ -55,82 +55,48 @@ public class MainActivity extends Activity {
         updateList();
 	}
 
-    private void updateList() {
-        UpdateListViewTask updateListViewTask = new UpdateListViewTask();
-        updateListViewTask.execute("");
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu items for use in the action bar
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_activity_menu, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 
-    private ArrayList<RssItem> getFeeds() throws SAXException, IOException,
-			MalformedURLException, XmlPullParserException {
-		ArrayList<String> feeds = new ArrayList<String>();
-		ArrayList<RssItem> rssItems = new ArrayList<RssItem>();
-		ArrayList<RssItem> feedItems = new ArrayList<RssItem>();
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle presses on the action bar items
+        switch (item.getItemId()) {
+            case R.id.action_bar_refresh:
+                updateList();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 
-		BufferedReader in = new BufferedReader(new InputStreamReader(this
-				.getResources().openRawResource(R.raw.feeds)));
-		String line;
-		while ((line = in.readLine()) != null) {
-			feeds.add(line);
-		}
+    private void updateList() {
+        UpdateListViewTask updateListViewTask = new UpdateListViewTask();
+        try {
+            updateListViewTask.execute(getFeeds());
+        } catch (IOException e) {
+            Crashlytics.logException(e);
+            Toast.makeText(getApplicationContext(), "Failed to read feeds list",
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
 
-		for (String s : feeds) {
-			feedItems = RssReader.read(new URL(s)).getRssItems();
-			checkDuplicates(rssItems, feedItems);
-		}
+    private String[] getFeeds() throws IOException {
+        ArrayList<String> feeds = new ArrayList<String>();
+        BufferedReader in = new BufferedReader(new InputStreamReader(this
+                .getResources().openRawResource(R.raw.feeds)));
+        String line;
+        while ((line = in.readLine()) != null) {
+            feeds.add(line);
+        }
 
-		Collections.sort(rssItems);// sorts into reverse date order
-		Collections.reverse(rssItems);// flip to correct order
-		return rssItems;
-	}
-
-	private void checkDuplicates(ArrayList<RssItem> rssItems,
-			ArrayList<RssItem> feedItems) {
-		for (RssItem y : feedItems) {
-			Boolean exists = false;
-			for (RssItem z : rssItems) {
-				if (z.getTitle().equalsIgnoreCase(y.getTitle())) {
-					exists = true;
-				}
-			}
-
-			if (!exists)
-				rssItems.add(y);
-		}
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu items for use in the action bar
-		MenuInflater inflater = getMenuInflater();
-		inflater.inflate(R.menu.main_activity_menu, menu);
-		return super.onCreateOptionsMenu(menu);
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle presses on the action bar items
-		switch (item.getItemId()) {
-		case R.id.action_bar_refresh:
-			updateList();
-			return true;
-		default:
-			return super.onOptionsItemSelected(item);
-		}
-	}
-
-	private HashMap<String, String> createStory(String title, String publishedDate) {
-		HashMap<String, String> story = new HashMap<String, String>();
-		story.put("title",title);
-        story.put("date",publishedDate);
-
-		return story;
-	}
-
-	private boolean isOnline() {
-        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-		NetworkInfo netInfo = cm.getActiveNetworkInfo();
-		return netInfo != null && netInfo.isConnected();
-	}
+        return feeds.toArray(new String[feeds.size()]);
+    }
 
     private class UpdateListViewTask extends AsyncTask<String,Void,Boolean>{
         private ProgressDialog dialog = new ProgressDialog(MainActivity.this);
@@ -147,7 +113,7 @@ public class MainActivity extends Activity {
             {
                 try{
                     //Set Lists
-                    Lists.RssItems = getFeeds();
+                    Lists.RssItems = getDataFromFeeds(feeds);
                     Lists.storyList = new ArrayList<Map<String,String>>();
                     ArrayList<RssItem> rssItems = new ArrayList<RssItem>();
 
@@ -181,7 +147,6 @@ public class MainActivity extends Activity {
                 return false;
             }
         }
-
         @Override
         protected void onPostExecute(Boolean result) {
             if(dialog.isShowing())
@@ -200,6 +165,50 @@ public class MainActivity extends Activity {
                 Toast.makeText(getApplicationContext(),
                         R.string.error_update_article_list, Toast.LENGTH_SHORT)
                         .show();
+            }
+        }
+
+        private boolean isOnline() {
+            ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo netInfo = cm.getActiveNetworkInfo();
+            return netInfo != null && netInfo.isConnected();
+        }
+
+        private HashMap<String, String> createStory(String title, String publishedDate) {
+            HashMap<String, String> story = new HashMap<String, String>();
+            story.put("title",title);
+            story.put("date",publishedDate);
+
+            return story;
+        }
+
+        private ArrayList<RssItem> getDataFromFeeds(String[] feeds) throws SAXException, IOException,
+                MalformedURLException, XmlPullParserException {
+            ArrayList<RssItem> rssItems = new ArrayList<RssItem>();
+            ArrayList<RssItem> feedItems = new ArrayList<RssItem>();
+
+            for (String s : feeds) {
+                feedItems = RssReader.read(new URL(s)).getRssItems();
+                checkDuplicates(rssItems, feedItems);
+            }
+
+            Collections.sort(rssItems);// sorts into reverse date order
+            Collections.reverse(rssItems);// flip to correct order
+            return rssItems;
+        }
+
+        private void checkDuplicates(ArrayList<RssItem> rssItems,
+                                     ArrayList<RssItem> feedItems) {
+            for (RssItem y : feedItems) {
+                Boolean exists = false;
+                for (RssItem z : rssItems) {
+                    if (z.getTitle().equalsIgnoreCase(y.getTitle())) {
+                        exists = true;
+                    }
+                }
+
+                if (!exists)
+                    rssItems.add(y);
             }
         }
     }
