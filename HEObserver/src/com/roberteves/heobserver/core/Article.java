7c4bdd47ce686685dcbbec6ee246aafc86edcfc5
Robert Eves <roberteves92@gmail.com>
2015-02-05 07:41:33 +0000

    implemented suggested solution http://stackoverflow.com/a/28335027/3666585

--- HEObserver/src/com/roberteves/heobserver/core/Article.java
+++ HEObserver/src/com/roberteves/heobserver/core/Article.java
@@ -1,7 +1,15 @@
 package com.roberteves.heobserver.core;
 
+import org.apache.http.Header;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Serializable;
 import java.net.URL;
@@ -14,6 +22,7 @@ import java.util.GregorianCalendar;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.zip.GZIPInputStream;
 
 import unbescape.html.HtmlEscape;
 
@@ -30,7 +39,7 @@ public class Article implements Serializable {
 	private static final String regexExcessWhitespace = "\\s+";
 	private static final String regexArticle = "<!-- Article Start -->([\\s\\S]*?)<!-- Article End -->";
 	private static final String regexHtml = "</?\\w+((\\s+\\w+(\\s*=\\s*(?:\".*?\"|'.*?'|[^'\">\\s]+))?)+\\s*|\\s*)/?>";
-	private static final String regexTitle = "<title>.*?<\\/title>";
+	private static final String regexTitle = "<title>\\s*.*?<\\/title>";
 	private static final String regexTitleStart = "<title>\\s+";
 	private static final String regexTitleEnd = "\\s\\|.*";
 
@@ -130,17 +139,33 @@ public class Article implements Serializable {
 	}
 
 	private static String getWebSource(String Url) throws IOException {
-		URL url = new URL(Url);
-		URLConnection urlConnection = url.openConnection();
-		BufferedReader br = new BufferedReader(new InputStreamReader(
-				urlConnection.getInputStream(), "UTF-8"));
-		String inputLine;
-		StringBuilder sb = new StringBuilder();
-		while ((inputLine = br.readLine()) != null)
-			sb.append(inputLine);
-		br.close();
+        HttpClient httpclient = new DefaultHttpClient(); // Create HTTP Client
+        HttpGet httpget = new HttpGet(Url); // Set the action you want to do
+        HttpResponse response = httpclient.execute(httpget); // Executeit
+        HttpEntity entity = response.getEntity();
 
-		return sb.toString();
+        InputStream is = response.getEntity().getContent();
+        Header contentEncoding = response.getFirstHeader("Content-Encoding");
+
+        BufferedReader reader;
+        if ((contentEncoding != null) && contentEncoding.getValue().equalsIgnoreCase("gzip")) {
+            InputStream gzipIs = new GZIPInputStream(is);
+            reader = new BufferedReader(new InputStreamReader(gzipIs), 8);
+        }
+        else {
+            reader = new BufferedReader(new InputStreamReader(is), 8);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        String line = null;
+        while ((line = reader.readLine()) != null) // Read line by line
+            sb.append(line + "\n");
+
+        String resString = sb.toString(); // Result is here
+
+        is.close(); // Close the stream
+
+        return resString;
 	}
 
 	public String getTitle() {
