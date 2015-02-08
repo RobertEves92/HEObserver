72135802c1ed8fa5beda5e9657ccc13c8d630554
Robert Eves <roberteves92@gmail.com>
2015-02-08 11:44:41 +0000

    created blank activity

---
+++ HEObserver/src/com/roberteves/heobserver/activities/AboutActivity.java
@@ -0,0 +1,14 @@
+package com.roberteves.heobserver.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.roberteves.heobserver.R;
+
+public class AboutActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+}
