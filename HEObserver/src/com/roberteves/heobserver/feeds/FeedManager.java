a12a8f31f37edffcb916ec47d751bf1a4f24bee7
Robert Eves <roberteves92@gmail.com>
2015-02-15 12:31:06 +0000

    Added local created of a feed list to overwrite Lists.FeedList with and crashlytics logging

--- HEObserver/src/com/roberteves/heobserver/feeds/FeedManager.java
+++ HEObserver/src/com/roberteves/heobserver/feeds/FeedManager.java
@@ -3,24 +3,33 @@ package com.roberteves.heobserver.feeds;
 
 import android.app.Activity;
 
+import com.crashlytics.android.Crashlytics;
 import com.roberteves.heobserver.R;
 import com.roberteves.heobserver.core.Lists;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class FeedManager {
     public static void LoadFeeds(Activity activity) {
+        ArrayList<Feed> feedList = new ArrayList<>();
         BufferedReader in = new BufferedReader(new InputStreamReader(activity.getApplicationContext()
                 .getResources().openRawResource(R.raw.feedsnew)));
         String line;
         Category c = Category.News;
-        while ((line = in.readLine()) != null) {
-            if (line.startsWith("#")) {
-                c = Feed.getCategoryFromString(line.toLowerCase().replace('#', '\0').trim());
-            } else {
-                Lists.FeedList.add(new Feed(line, c));
+        try {
+            while ((line = in.readLine()) != null) {
+                if (line.startsWith("#")) {
+                    c = Feed.getCategoryFromString(line.toLowerCase().replace('#', '\0').trim());
+                } else {
+                    feedList.add(new Feed(line, c));
+                }
             }
+            Lists.FeedList = feedList;
+        } catch (IOException e) {
+            Crashlytics.logException(e);
         }
     }
 }
