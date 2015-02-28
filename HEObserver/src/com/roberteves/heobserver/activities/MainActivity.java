20b466ac9ff9ccc32bd62fe30630c948a383a01a
Robert Eves <roberteves92@gmail.com>
2015-02-28 15:50:38 +0000

    externalised constructor and renamed

--- HEObserver/src/com/roberteves/heobserver/activities/MainActivity.java
+++ HEObserver/src/com/roberteves/heobserver/activities/MainActivity.java
@@ -40,6 +40,7 @@ import unbescape.html.HtmlEscape;
 
 public class MainActivity extends Activity {
     private static ListView lv;
+    private static ProgressDialog articleDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,7 +50,11 @@ public class MainActivity extends Activity {
         setTitle(getString(R.string.app_name_long));
         setContentView(R.layout.activity_scroll_list);
         lv = (ListView) findViewById(R.id.listView);
-
+        
+        articleDialog = new ProgressDialog(MainActivity.this);
+        articleDialog.setMessage("Loading Article...");
+        articleDialog.setCancelable(false);
+        
         updateList();
     }
 
@@ -113,17 +118,12 @@ public class MainActivity extends Activity {
                 if (isOnline()) {
                     Article article;
                     try {
-                        ProgressDialog dialog = new ProgressDialog(MainActivity.this);
-                        dialog.setMessage("Loading Article...");
-                        dialog.setCancelable(false);
-                        dialog.show();
-                        
                         //Load article
+                        articleDialog.show();
                         article = new Article(Lists.RssItems.get(position)
                                 .getLink(), Lists.RssItems.get(
                                 position).getPubDate());
-
-                        dialog.cancel();
+                        articleDialog.cancel();
                         
                         Intent i = new Intent(MainActivity.this,
                                 ArticleActivity.class);
