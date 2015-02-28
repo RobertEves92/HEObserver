59832d418ee64fb0a9ca9c507a10f7d2a3310428
Robert Eves <roberteves92@gmail.com>
2015-02-28 17:23:05 +0000

    Fixed class structure

--- HEObserver/src/com/roberteves/heobserver/activities/CommentActivity.java
+++ HEObserver/src/com/roberteves/heobserver/activities/CommentActivity.java
@@ -14,13 +14,12 @@ import java.util.List;
 import java.util.Map;
 
 public class CommentActivity extends Activity {
-    private static ListView lv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scroll_list);
-        lv = (ListView) findViewById(R.id.listView);
+        ListView lv = (ListView) findViewById(R.id.listView);
 
         ArrayList<Comment> comments = (ArrayList<Comment>) getIntent().getSerializableExtra("comments");
         List<Map<String, String>> commentList;
