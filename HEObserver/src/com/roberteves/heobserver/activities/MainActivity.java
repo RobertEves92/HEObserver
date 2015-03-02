1268cefc950f668831b83af23fd97c281d265012
Robert Eves <roberteves92@gmail.com>
2015-03-02 00:04:38 +0000

    removed unused import

--- HEObserver/src/com/roberteves/heobserver/activities/MainActivity.java
+++ HEObserver/src/com/roberteves/heobserver/activities/MainActivity.java
@@ -8,7 +8,6 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
