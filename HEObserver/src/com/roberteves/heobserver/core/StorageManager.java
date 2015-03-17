845cfa3496d4b6f6f7f6370a2625474fdfea1bd8
Robert Eves <roberteves92@gmail.com>
2015-03-17 16:12:23 +0000

    used foreach for storing, added loading of story list by loop

--- HEObserver/src/com/roberteves/heobserver/core/StorageManager.java
+++ HEObserver/src/com/roberteves/heobserver/core/StorageManager.java
@@ -2,10 +2,11 @@ package com.roberteves.heobserver.core;
 
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.SharedPreferences;
 
-import java.util.HashSet;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class StorageManager {
@@ -17,23 +18,38 @@ public class StorageManager {
         settings = activity.getApplicationContext().getSharedPreferences(PREFS_NAME, 0);
         editor = settings.edit();
 
+        //region StoryList
         editor.putInt("storylist_size", Lists.storyList.size());
 
         int i = 0;
-        for(Map<String,String> m : Lists.storyList)
-        {
-            editor.putString("storylist_"+i+"_date",m.get("date"));
-            editor.putString("storylist_"+i+"_title",m.get("title"));
+        for (Map<String, String> m : Lists.storyList) {
+            editor.putString("storylist_" + i + "_date", m.get("date"));
+            editor.putString("storylist_" + i + "_title", m.get("title"));
             i++;
         }
-        editor.commit();
+        //endregion
+
+        editor.apply();
     }
 
-    public static Map<?, ?> GetData(Activity activity)
-    {
+    public static void GetData(Activity activity) {
         SharedPreferences settings;
         settings = activity.getApplicationContext().getSharedPreferences(PREFS_NAME, 0);
-        Map<?,?> map = settings.getAll();
-        return map;
+
+        //region StoryList
+        int storyListSize = settings.getInt("storylist_size", 0);
+        List<Map<String, String>> storyList = new ArrayList<>();
+
+        int i = 0;
+        do {
+            HashMap<String, String> story = new HashMap<>();
+            story.put("title", settings.getString("storylist_" + i + "_title", ""));
+            story.put("date", settings.getString("storylist_" + i + "_date", ""));
+            storyList.add(story);
+            i++;
+        } while (i != storyListSize);
+        //endregion
+
+        //TODO add final processing here
     }
 }
