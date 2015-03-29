085f59a9537a6591bdc7e10caedf8399c0d9650d
Robert Eves <roberteves92@gmail.com>
2015-03-29 02:16:27 +0100

    update and display view before checking for updates

--- HEObserver/src/com/roberteves/heobserver/activities/MainActivity.java
+++ HEObserver/src/com/roberteves/heobserver/activities/MainActivity.java
@@ -56,10 +56,9 @@ public class MainActivity extends Activity {
 
         //Display saved feeds if available or update and display if not or 1hr since last update
         if (StorageManager.LoadLists(MainActivity.this)) {
+            UpdateView();
             if (CheckUpdates()) {
                 updateList();
-            } else {
-                UpdateView();
             }
         } else {
             updateList();
