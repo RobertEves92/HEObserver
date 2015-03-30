33a3f837e1b4ffb871eb8a6c62e2cd9d9660bc2f
Robert Eves <roberteves92@gmail.com>
2015-03-30 16:59:19 +0100

    added logging to markdown activity

--- HEObserver/src/com/roberteves/heobserver/activities/MarkdownActivity.java
+++ HEObserver/src/com/roberteves/heobserver/activities/MarkdownActivity.java
@@ -3,6 +3,8 @@ package com.roberteves.heobserver.activities;
 import android.app.Activity;
 import android.os.Bundle;
 
+import com.roberteves.heobserver.core.Util;
+
 import us.feras.mdv.MarkdownView;
 
 public class MarkdownActivity extends Activity {
@@ -10,10 +12,16 @@ public class MarkdownActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Util.LogMessage("MarkdownActivity", "Activity Ended");
         MarkdownView markdownView = new MarkdownView(this);
         setContentView(markdownView);
         setTitle(getIntent().getStringExtra("title"));
         markdownView.loadMarkdownFile(getIntent().getStringExtra("url"));
-        //markdownView.loadMarkdownFile("https://raw.githubusercontent.com/RobertEves92/HEObserver/master/README.md");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Util.LogMessage("MarkdownActivity", "Activity Ended");
     }
 }
