a150637e6521183145758dd64053a896931d64f1
Robert Eves <roberteves92@gmail.com>
2017-02-25 08:37:54 +0000

    replaced blanked method with new method

--- HEObserver/src/com/roberteves/heobserver/core/Util.java
+++ HEObserver/src/com/roberteves/heobserver/core/Util.java
@@ -70,7 +70,22 @@ public class Util {
     }
 
     public static String getWebSource(String Url) throws IOException {
+        HttpClient httpclient = new DefaultHttpClient(); // Create HTTP Client
+        HttpGet httpget = new HttpGet(Url); // Set the action you want to do
+        HttpResponse response = httpclient.execute(httpget); // Executeit
+        HttpEntity entity = response.getEntity();
+        InputStream is = entity.getContent(); // Create an InputStream with the response
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is, "iso-8859-1"), 8);
+        StringBuilder sb = new StringBuilder();
+        String line = null;
+        while ((line = reader.readLine()) != null) // Read line by line
+            sb.append(line + "\n");
 
+        String resString = sb.toString(); // Result is here
+
+        is.close(); // Close the stream
+
+        return resString;
     }
 
     public static void LogException(String action, String data, Exception e) {
